170833a2523d1f7663a058ce22ae0e500c3eac48
PjatkUserSupreme <s24817@pjwstk.edu.pl>
2023-03-27 16:02:37 +0200

    Cw1

---
+++ code4_1.py
@@ -0,0 +1,13 @@
+import math
+
+
+def panel_calc(dlug_podlog, szer_podlog, dlug_panel, szer_panel, panel_w_opak):
+    powierzchnia_podloga = (szer_podlog * dlug_podlog) * 1.1
+    print(powierzchnia_podloga)
+    powierzchnia_panel = szer_panel * dlug_panel
+    print(powierzchnia_panel)
+    ile_paneli = math.ceil(powierzchnia_podloga/powierzchnia_panel)
+    ile_opak = math.ceil(ile_paneli/panel_w_opak)
+    return ile_opak
+
+print(("Potrzeba : " + str(panel_calc(4, 4, 0.20, 1, 10))))
